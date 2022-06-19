bcd75f8fb75fa7919926826ad159820af1fbe98b
Alexander Locke <alocke692@gmail.com>
2022-06-19 15:03:26 -0400

    Character controller so far
    Added basic character controller, URP package, Pixel Perfect camera and anims

---
+++ Zombie Rush/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float zoomSize = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+        
+  
+
+}
