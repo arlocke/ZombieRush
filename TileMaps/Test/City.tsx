2fb974b85c1b5cfcd55ca817d2873b508749c9b9
Matt Thompson <matthewsmiththompson@gmail.com>
2022-12-12 02:19:34 -0500

    Tiled map test
    Added tiled map importing addon
    Imported a map

---
+++ TileMaps/Test/City.tsx
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="City" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../Sprites/Environment/Tilesets/Road_SpriteMap_2.0.png" width="512" height="512"/>
+ <tile id="224">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="32">
+    <polygon points="0,0 30,0 30,24 15,31 0,24"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="225">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="32">
+    <polygon points="0,0 30,0 30,24 15,31 0,24"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="226">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="32">
+    <polygon points="0,0 30,0 30,24 15,31 0,24"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="227">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="32">
+    <polygon points="0,0 30,0 30,24 15,31 0,24"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Ground" type="corner" tile="-1">
+   <wangcolor name="Road" color="#474858" tile="-1" probability="1"/>
+   <wangcolor name="CobbleSidewalk" color="#d6d1c1" tile="-1" probability="1"/>
+   <wangcolor name="Grass" color="#4f9c3b" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="1" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="2" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="3" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="4" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="5" wangid="0,3,0,2,0,3,0,3"/>
+   <wangtile tileid="6" wangid="0,3,0,3,0,2,0,3"/>
+   <wangtile tileid="7" wangid="0,2,0,3,0,2,0,2"/>
+   <wangtile tileid="8" wangid="0,2,0,3,0,3,0,2"/>
+   <wangtile tileid="9" wangid="0,2,0,2,0,3,0,2"/>
+   <wangtile tileid="10" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="17" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="18" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="19" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="20" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="21" wangid="0,2,0,3,0,3,0,3"/>
+   <wangtile tileid="22" wangid="0,3,0,3,0,3,0,2"/>
+   <wangtile tileid="23" wangid="0,3,0,3,0,2,0,2"/>
+   <wangtile tileid="24" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="25" wangid="0,2,0,2,0,3,0,3"/>
+   <wangtile tileid="34" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="35" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="36" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="39" wangid="0,3,0,2,0,2,0,2"/>
+   <wangtile tileid="40" wangid="0,3,0,2,0,2,0,3"/>
+   <wangtile tileid="41" wangid="0,2,0,2,0,2,0,3"/>
+   <wangtile tileid="57" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="73" wangid="0,3,0,3,0,3,0,3"/>
+  </wangset>
+ </wangsets>
+</tileset>
